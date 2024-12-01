70a82944582bf3903f866993d93e8519a63efc81
Alejandro Diaz Asencio <alejandromdiaz89@gmail.com>
2024-11-30 22:30:06 -0300

    Fix parte 2 input NaN

--- assets/js/parte2.js
+++ assets/js/parte2.js
@@ -6,9 +6,9 @@ verificar = document.querySelector(".boton");
 alerta = document.querySelector(".mensaje");
 
 verificar.addEventListener("click",() => {
-    cantidad1 = parseInt(sticker1.value);
-    cantidad2 = parseInt(sticker2.value);
-    cantidad3 = parseInt(sticker3.value);
+    cantidad1 = parseInt(sticker1.value)|| 0;
+    cantidad2 = parseInt(sticker2.value)|| 0;
+    cantidad3 = parseInt(sticker3.value)|| 0;
     total = cantidad1 + cantidad2 + cantidad3;
 
     if (total > 10) {
